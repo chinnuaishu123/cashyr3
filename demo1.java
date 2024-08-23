76abb92db1b6fa3265f4107ffdc2f028f3ee486a
chinnuaishu123 <chinmayeeschinmayee4@gmail.com>
2024-08-23 11:24:18 +0530

    Add files via upload

---
+++ demo1.java
@@ -0,0 +1,14 @@
+import java.unit.scanner
+class demo1{
+public static void main(string[]arres){
+scanner sc=new scanner(System ln);
+system.out.print("enter the marks");
+int marks=sc.next int()
+if(marks)=60{{marks<=100}}){
+system.out.print ln("your grade is;A")
+}
+else if (marks>=50{{marks<60}}){
+system.out.print ln("your grade isB")
+}
+}
+}
